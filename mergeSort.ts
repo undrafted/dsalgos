fef43b82a9bd46a5c005d9d0293fba9f84fad802
undrafted <8jermiedomingo@gmail.com>
2020-05-02 03:06:37 +0200

    add merge sort

--- mergeSort.ts
+++ mergeSort.ts
@@ -1,8 +1,8 @@
 import { strict as assert } from "assert";
 
+// https://www.youtube.com/watch?v=JSceec-wEyw
 const mergeSort = (arr: number[]): number[] => {
   if (arr.length < 2) {
-    console.log(arr);
     return arr;
   }
 
@@ -28,12 +28,10 @@ const merge = (left: number[], right: number[]): number[] => {
 
   // spread left and right too for cases where left is empty and right is not and vice versa
   const result = [...sorted, ...left, ...right];
-  console.log(result);
   return result;
 };
 
 const arr = [10, 5, 2, 1, 4, 3, 7, 9, 8, 6];
 console.log(arr);
 console.log(mergeSort(arr));
-
 assert.deepEqual(mergeSort(arr), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
